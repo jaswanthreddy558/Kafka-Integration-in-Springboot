cb810ced699d76c096e4f7e00c71a07b0fe167fa
Jaswanth Kesar <@11K81e0019>
2023-11-02 11:23:54 -0400

    Complete Kafka Producer API logic.

--- src/main/java/com/example/kafka/consumer/MessageConsumer.java
+++ src/main/java/com/example/kafka/consumer/MessageConsumer.java
@@ -1,12 +1,19 @@
 package com.example.kafka.consumer;
 
+import com.example.kafka.model.KafkaMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageConsumer {
-    @KafkaListener(topics = "KAFKATOPIC", groupId = "my-group-id")
-    public void listen(String message) {
-        System.out.println("Received message: " + message);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @KafkaListener(topics = "users", groupId = "${spring.kafka.consumer.group-id}")
+    public void listen(@Payload KafkaMessage kafkaMessage) {
+        LOGGER.info("Message received ID : {} Message Contact Number: {} Message Email : {}",
+                kafkaMessage.getId(), kafkaMessage.getContactNo(), kafkaMessage.getEmail());
     }
 }
